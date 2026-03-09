184a771d3acbde404cb473f4c7d56b35d45cf91f
freelancingtips <vesaljan66@gmail.com>
2026-03-09 20:48:20 +0500

    Add address, email, and phone number generators
    Added functions to generate address, email, and phone number.

--- js/generator.js
+++ js/generator.js
@@ -4,3 +4,15 @@ function generateName(gender = "any") {
     const last = randomItem(surnames);
     return first + " " + last;
 }
+
+function generateAddress() {
+    return randomNumber(10,9999) + " " + randomItem(streets);
+}
+
+function generateEmail(name) {
+    return name.toLowerCase().replace(" ", ".") + "@" + randomItem(domains);
+}
+
+function generatePhone() {
+    return "+1 "+randomNumber(200,999)+"-"+randomNumber(100,999)+"-"+randomNumber(1000,9999);
+}
