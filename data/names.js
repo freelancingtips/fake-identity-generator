9589c6d90f9d4b9222951f81b5b43b5a89d2fed6
freelancingtips <vesaljan66@gmail.com>
2026-03-09 23:40:46 +0500

    Update names.js

--- data/names.js
+++ data/names.js
@@ -1,23 +1,32 @@
-const names = [
-  { name: "James", gender: "male" },
-  { name: "John", gender: "male" },
-  { name: "Robert", gender: "male" },
-  { name: "Michael", gender: "male" },
-  { name: "William", gender: "male" },
-  { name: "David", gender: "male" },
-  { name: "Richard", gender: "male" },
-  { name: "Mary", gender: "female" },
-  { name: "Patricia", gender: "female" },
-  { name: "Jennifer", gender: "female" },
-  { name: "Linda", gender: "female" },
-  { name: "Elizabeth", gender: "female" },
-  { name: "Susan", gender: "female" },
-  { name: "Jessica", gender: "female" },
-  { name: "Charles", gender: "male" },
-  { name: "Joseph", gender: "male" },
-  { name: "Thomas", gender: "male" },
-  { name: "Sarah", gender: "female" },
-  { name: "Karen", gender: "female" },
-  { name: "Nancy", gender: "female" }
-  // ...continue until 3000 entries
+const maleNames = [
+"James","John","Robert","Michael","William","David","Richard","Charles","Joseph","Thomas",
+"Christopher","Daniel","Paul","Mark","Donald","George","Kenneth","Steven","Edward","Brian",
+"Ronald","Anthony","Kevin","Jason","Matthew","Gary","Timothy","Jose","Larry","Jeffrey","Frank",
+"Scott","Eric","Stephen","Andrew","Raymond","Gregory","Joshua","Jerry","Dennis","Walter",
+"Patrick","Peter","Harold","Douglas","Henry","Carl","Arthur","Ryan","Roger","Joe",
+"Juan","Jack","Albert","Jonathan","Justin","Terry","Gerald","Keith","Samuel","Willie",
+"Ralph","Lawrence","Nicholas","Roy","Benjamin","Bruce","Brandon","Adam","Harry","Fred",
+"Wayne","Billy","Steve","Louis","Jeremy","Aaron","Randy","Howard","Eugene","Carlos",
+"Russell","Bobby","Victor","Martin","Ernest","Phillip","Todd","Jesse","Craig","Alan",
+"Shawn","Clarence","Sean","Philip","Chris","Johnny","Earl","Jimmy","Antonio","Danny",
+"Bryan","Tony","Luis","Mike","Stanley","Leonard","Nathan","Dale","Manuel","Rodney",
+"Curtis","Norman","Allen","Marvin","Vincent","Glenn","Jeffery","Travis","Jeff","Chad",
+"Jacob","Lee","Melvin","Alfred","Kyle","Francis","Bradley","Jesus","Herbert","Frederick",
+"Ray","Joel","Edwin","Don","Eddie","Ricky","Troy","Randall","Barry","Alexander",
+"Bernard","Mario","Leroy","Francisco","Marcus","Micheal","Theodore","Clifford","Miguel","Oscar",
+// ... continue pattern until ~1500 names
+];
+
+const femaleNames = [
+"Mary","Patricia","Jennifer","Linda","Elizabeth","Susan","Jessica","Sarah","Karen","Nancy",
+"Lisa","Betty","Margaret","Sandra","Ashley","Kimberly","Emily","Donna","Michelle","Dorothy",
+"Carol","Amanda","Melissa","Deborah","Stephanie","Rebecca","Laura","Sharon","Cynthia","Kathleen",
+"Helen","Amy","Shirley","Angela","Anna","Brenda","Pamela","Nicole","Ruth","Katherine",
+"Samantha","Christine","Emma","Catherine","Debra","Virginia","Rachel","Carolyn","Janet","Maria",
+"Heather","Diane","Julie","Joyce","Victoria","Kelly","Christina","Lauren","Joan","Evelyn",
+"Judith","Megan","Cheryl","Andrea","Hannah","Jacqueline","Martha","Gloria","Teresa","Ann",
+"Sara","Madison","Frances","Kathryn","Janice","Jean","Abigail","Alice","Julia","Judy",
+"Grace","Denise","Amber","Doris","Marilyn","Beverly","Danielle","Theresa","Sophia","Marie",
+"Diana","Brittany","Natalie","Isabella","Charlotte","Rose","Alexis","Kayla","Lori","Jane",
+// ... continue pattern until ~1500 names
 ];
