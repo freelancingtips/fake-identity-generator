ed1c71d3497e8f98745349318d6bf05fea088778
freelancingtips <vesaljan66@gmail.com>
2026-03-09 20:50:26 +0500

    Add names array with gender details
    Added an array of names with gender information.

--- data/names.js
+++ data/names.js
@@ -1,1 +1,23 @@
-
+const names = [
+  { name: "James", gender: "male" },
+  { name: "John", gender: "male" },
+  { name: "Robert", gender: "male" },
+  { name: "Michael", gender: "male" },
+  { name: "William", gender: "male" },
+  { name: "David", gender: "male" },
+  { name: "Richard", gender: "male" },
+  { name: "Mary", gender: "female" },
+  { name: "Patricia", gender: "female" },
+  { name: "Jennifer", gender: "female" },
+  { name: "Linda", gender: "female" },
+  { name: "Elizabeth", gender: "female" },
+  { name: "Susan", gender: "female" },
+  { name: "Jessica", gender: "female" },
+  { name: "Charles", gender: "male" },
+  { name: "Joseph", gender: "male" },
+  { name: "Thomas", gender: "male" },
+  { name: "Sarah", gender: "female" },
+  { name: "Karen", gender: "female" },
+  { name: "Nancy", gender: "female" }
+  // ...continue until 3000 entries
+];
