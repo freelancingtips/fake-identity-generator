731785c368bb55a1f80175649ea043ce5e9655b9
freelancingtips <vesaljan66@gmail.com>
2026-03-09 20:39:54 +0500

    Add utility functions for random generation

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,24 @@
+function randomItem(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
 
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateUsername(name) {
+    return name.toLowerCase().replace(" ", ".") + randomNumber(10, 99);
+}
+
+function generatePassword(length=8) {
+    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
+    let pass = "";
+    for (let i=0;i<length;i++){
+        pass += chars.charAt(Math.floor(Math.random()*chars.length));
+    }
+    return pass;
+}
+
+function generateCreditCard(){
+    return randomNumber(4000,4999)+"-"+randomNumber(1000,9999)+"-"+randomNumber(1000,9999)+"-"+randomNumber(1000,9999);
+}
