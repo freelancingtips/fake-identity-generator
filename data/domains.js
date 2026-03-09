e9c5c2538bd11c8c3b260c00fecfbbba9dfb152c
freelancingtips <vesaljan66@gmail.com>
2026-03-09 20:55:30 +0500

    Add list of email domains to domains.js

--- data/domains.js
+++ data/domains.js
@@ -1,1 +1,10 @@
-
+const domains = [
+"gmail.com","yahoo.com","outlook.com","mail.com","hotmail.com","aol.com",
+"icloud.com","protonmail.com","zoho.com","yandex.com","gmx.com","live.com",
+"msn.com","fastmail.com","inbox.com","mail.ru","qq.com","163.com","126.com",
+"tutanota.com","hushmail.com","me.com","optonline.net","bellsouth.net","cox.net",
+"sbcglobal.net","att.net","verizon.net","comcast.net","earthlink.net","mac.com",
+"mailfence.com","runbox.com","posteo.net","rediffmail.com","lycos.com","mailinator.com",
+"seznam.cz","web.de","orange.fr","wanadoo.fr","btinternet.com","btopenworld.com",
+"bigpond.com","telus.net","shaw.ca","rogers.com","sky.com","live.co.uk"
+];
