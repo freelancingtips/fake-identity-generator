95f7e5d2946934ec36a229cb024f1f58ed3d8e58
freelancingtips <vesaljan66@gmail.com>
2026-03-09 20:47:10 +0500

    Refactor generateName function and remove unused functions
    Refactor generateName function to support any gender and filter names accordingly. Removed unused functions for address, email, and phone generation.

--- js/generator.js
+++ js/generator.js
@@ -1,17 +1,6 @@
-function generateName(gender){
-    const first = (gender==="female") ? randomItem(femaleNames) : randomItem(maleNames);
+function generateName(gender = "any") {
+    let filtered = (gender === "any") ? names : names.filter(n => n.gender === gender);
+    const first = randomItem(filtered).name;
     const last = randomItem(surnames);
     return first + " " + last;
-}
-
-function generateAddress(){
-    return randomNumber(10,9999) + " " + randomItem(streets);
-}
-
-function generateEmail(name){
-    return name.toLowerCase().replace(" ",".") + "@" + randomItem(domains);
-}
-
-function generatePhone(){
-    return "+1 "+randomNumber(200,999)+"-"+randomNumber(100,999)+"-"+randomNumber(1000,9999);
 }
