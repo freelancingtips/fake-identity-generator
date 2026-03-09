e23de36e917123020bab2123c9b273ddf06e155c
freelancingtips <vesaljan66@gmail.com>
2026-03-09 23:44:57 +0500

    Update domains.js

--- data/domains.js
+++ data/domains.js
@@ -1,10 +1,15 @@
 const domains = [
-"gmail.com","yahoo.com","outlook.com","mail.com","hotmail.com","aol.com",
-"icloud.com","protonmail.com","zoho.com","yandex.com","gmx.com","live.com",
-"msn.com","fastmail.com","inbox.com","mail.ru","qq.com","163.com","126.com",
-"tutanota.com","hushmail.com","me.com","optonline.net","bellsouth.net","cox.net",
-"sbcglobal.net","att.net","verizon.net","comcast.net","earthlink.net","mac.com",
-"mailfence.com","runbox.com","posteo.net","rediffmail.com","lycos.com","mailinator.com",
-"seznam.cz","web.de","orange.fr","wanadoo.fr","btinternet.com","btopenworld.com",
-"bigpond.com","telus.net","shaw.ca","rogers.com","sky.com","live.co.uk"
+"gmail.com","yahoo.com","outlook.com","mail.com","hotmail.com","aol.com","icloud.com",
+"protonmail.com","yandex.com","gmx.com","zoho.com","live.com","msn.com","inbox.com",
+"fastmail.com","hushmail.com","tutanota.com","rocketmail.com","att.net","comcast.net",
+"verizon.net","btinternet.com","sky.com","orange.fr","free.fr","web.de","mail.ru",
+"qq.com","163.com","126.com","naver.com","daum.net","seznam.cz","rediffmail.com",
+"indiatimes.com","sbcglobal.net","bellsouth.net","cox.net","charter.net","shaw.ca",
+"rogers.com","telus.net","optusnet.com.au","bigpond.com","tiscali.it","libero.it",
+"virgilio.it","laposte.net","wanadoo.fr","yahoo.co.uk","yahoo.ca","yahoo.fr","yahoo.de",
+"yahoo.es","yahoo.co.in","yahoo.com.au","gmail.co.uk","gmail.ca","gmail.de","gmail.fr",
+"gmail.co.in","outlook.co.uk","outlook.ca","outlook.de","outlook.fr","outlook.co.in",
+"mailinator.com","example.com","testmail.com","email.com","myself.com","post.com",
+"consultant.com","engineer.com","accountant.com","techie.com","dr.com","usa.com",
+"asia.com","europe.com","africa.com","iname.com","writeme.com","workmail.com"
 ];
