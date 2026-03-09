0f15735d22b7f518f1b8e8b668c0ac29aaa562c0
freelancingtips <vesaljan66@gmail.com>
2026-03-09 20:40:39 +0500

    Add functions to generate name, address, email, and phone

--- js/generator.js
+++ js/generator.js
@@ -1,1 +1,17 @@
+function generateName(gender){
+    const first = (gender==="female") ? randomItem(femaleNames) : randomItem(maleNames);
+    const last = randomItem(surnames);
+    return first + " " + last;
+}
 
+function generateAddress(){
+    return randomNumber(10,9999) + " " + randomItem(streets);
+}
+
+function generateEmail(name){
+    return name.toLowerCase().replace(" ",".") + "@" + randomItem(domains);
+}
+
+function generatePhone(){
+    return "+1 "+randomNumber(200,999)+"-"+randomNumber(100,999)+"-"+randomNumber(1000,9999);
+}
